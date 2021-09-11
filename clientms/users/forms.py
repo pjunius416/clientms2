3f2163f09e31cefb5db2c6970c6e77285656362c
pjunius416 <52839119+pjunius416@users.noreply.github.com>
2021-09-11 12:01:01 -0500

    Completed Section 6

--- clientms/users/forms.py
+++ clientms/users/forms.py
@@ -1,12 +1,14 @@
-from django import forms;
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-    model = CustomUser
-    fields = ('username', 'email', 'department', 'employee_cell_phone')
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ('username', 'email', 'department', 'employee_cell_phone')
     
 class CustomUserChangeForm(UserChangeForm):
-    model = CustomUser
-    fields = ('username', 'email', 'department', 'employee_cell_phone')
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'department', 'employee_cell_phone')
